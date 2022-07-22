87c6ebdd98d4c81b3ed6831005844cdb1435b706
Saravanan M <sarvan.m20@gmail.com>
2022-07-22 19:06:43 +0530

    Able to show uploaded image

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FileUpload from './Components/FileUpload.js';
+
+const App = () => (
+    <div className="container mt-4">
+      <h4 className='display-4 text-center mb-4'>
+        File upload
+      </h4>
+      <FileUpload />
+    </div>
+  );
+
+export default App;
